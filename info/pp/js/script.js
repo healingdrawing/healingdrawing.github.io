6c57ef0e31ff78a9e49bd663e0dcb90eec13654b
healingdrawing <healingdrawing@gmail.com>
2019-10-11 21:40:15 +0300

    pp

--- info/pp/js/script.js
+++ info/pp/js/script.js
@@ -1,20 +1,19 @@
 function generate(){
     var parameters = new URLSearchParams(window.location.search)
-    var product = 'product test'
-    var creator = 'creator test'
+    var product = 'add product name as url param product=appname'
+    var creator = 'add developer name as url param creator=devname'
+    var contacts = 'add contacts adress as url param contacts=adress' //kyznector☭gmail.com
     if(parameters.has('product')) product = parameters.get('product')
     if(parameters.has('creator')) creator = parameters.get('creator')
+    if(parameters.has('contacts')) contacts = parameters.get('contacts')
     //replace tag inner html
-    alert(product)
-    alert(creator)
-    if(product != ''){
-        var tags = document.body.getElementsByClassName('product')
-        for (var i = 0;i< tags.length;i++){tags[i].innerHTML = product}
-    }
-    if(creator != ''){
-        var tags = document.body.getElementsByClassName('creator')
-        for (var i = 0;i< tags.length;i++){tags[i].innerHTML = creator}
-    }
+    var tags = document.body.getElementsByClassName('product')
+    for (var i = 0;i< tags.length;i++){tags[i].innerHTML = product}
+    var tags = document.body.getElementsByClassName('creator')
+    for (var i = 0;i< tags.length;i++){tags[i].innerHTML = creator}
+    var tags = document.body.getElementsByClassName('contacts')
+    for (var i = 0;i< tags.length;i++){tags[i].innerHTML = contacts}
+    
 }
 
 generate()
