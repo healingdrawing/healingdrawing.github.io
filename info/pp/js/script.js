bad8e7433a07eb7a2f4b31e56eb41df432979f30
healingdrawing <healingdrawing@gmail.com>
2019-10-11 20:02:59 +0300

    privacy policy

--- info/pp/js/script.js
+++ info/pp/js/script.js
@@ -2,11 +2,15 @@ function generate(){
     var parameters = new URLSearchParams(window.location.search)
     var product = parameters.get('product')
     var creator = parameters.get('creator')
-    //replace product tag inner html
-    var tags = document.body.getElementsByClassName('product')
-    for (var i = 0;i< tags.length;i++){tags[i].innerHTML = product}
-    var tags = document.body.getElementsByClassName('creator')
-    for (var i = 0;i< tags.length;i++){tags[i].innerHTML = creator}
+    //replace tag inner html
+    if(product != ''){
+        var tags = document.body.getElementsByClassName('product')
+        for (var i = 0;i< tags.length;i++){tags[i].innerHTML = product}
+    }
+    if(creator != ''){
+        var tags = document.body.getElementsByClassName('creator')
+        for (var i = 0;i< tags.length;i++){tags[i].innerHTML = creator}
+    }
 }
 
 generate()
