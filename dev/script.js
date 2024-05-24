b7914eba1b31d9115885295521d1bd88e0e29f6e
healingdrawing <healingdrawing@gmail.com>
2024-05-24 04:27:11 +0300

    remove extra spaces , and replace < > in html to &lt; &gt;

--- dev/script.js
+++ dev/script.js
@@ -58,8 +58,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // Modify the text content by removing the minimum indentation
     // var modifiedText = element.textContent.split('\n').slice(1, -1).join('<br>\n').replace(new RegExp(`^\\s{${minIndent}}`, 'gm'), '');
     var modifiedText = element.textContent
-    .replaceAll(" > ","&#65125;")
-    .replaceAll(" < ","&#65124;")
+    .replaceAll(">","&#65125;")
+    .replaceAll("<","&#65124;")
     // now we can display jsx, but can not copypaste code without fix replacements.
     .split('\n').slice(1, -1).map(text => `<div class="cx">${text}</div>`).join('').replace(new RegExp(`<div class="cx">\\s{${minIndent}}`, 'gm'), '<div class="cx">');
     
