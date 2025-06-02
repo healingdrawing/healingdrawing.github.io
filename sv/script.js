08198d36bd1ccad62f8eaedada08acd95503b0ab
healingdrawing <healingdrawing@gmail.com>
2025-06-02 20:44:39 +0300

    wip formatting of group of single words

--- sv/script.js
+++ sv/script.js
@@ -68,98 +68,18 @@ function swedish_numbers(){
 }
 
 /** mode is string */
-function parse_all(mode = "original"){
-  let number = 1;
-  let text_number = 1; // to visually separate, more than one sentence sequence.
-  let text_section = true; // to separate text using new line
-  let word_section = true; // to separate word + text using hr
-  // parse
-  var all_split = content.innerHTML.trim().split("\n\n");
-  console.log("all_split before sort_words", all_split, "mode before sort_words", mode);
-  all_split = sort_words(all_split, mode);
+function parse_all(mode = "show all"){
   
-  var filtered = "";
-  for (i=0;i<all_split.length;i++){
-    if (all_split[i].startsWith("[t] ")){ // text/phrase found
-      
-      // check for long text
-      let [sentences, translations] = all_split[i].split("[t] ")[1].split("\n");
-      sentences = sentences.split("  ");
-      translations = translations.split("  ");
-      
-      if (translations.length !== sentences.length) {
-        console.error("incorrect sentences/translations batch found", sentences, translations);
-        continue;
-      }
-      
-      if (sentences.length > 1){
-        word_section = true
-        text_section = false;
-        filtered += '<div class="separator"><div class="line"></div><div class="marker">'
-        + '📖 '
-        + text_number++
-        + '</div><div class="line"></div></div>';
-      }
-      else if (text_section) {
-        word_section = true
-        text_section = false;
-        filtered += '<br>'; // case of new line after words
-      }
-      
-      for (let i=0;i< sentences.length;i++){
-        let sen = sentences[i].trim();
-        let tra = translations[i].trim();
-        if (sen === "" || tra === "") {
-          console.error("empty sentence/translation pair found", sentences, translations);
-          continue;
-        }
-        filtered += '<div class="text" onclick="show_hide_translation(this)">'
-        + '<div class="sv">' + sen + '</div>'
-        + '<div class="en back">' + tra + '</div></div>';
-      }
-      
-      // if (sentences.length > 1) filtered += '<div class="separator"><div class="line"></div><div class="marker">💆🏻</div><div class="line"></div></div>';
-    }else{ //word found
-      
-      if (word_section) {
-        text_section = true;
-        word_section = false;
-        filtered += '<div class="separator"><div class="line"></div><div class="number">section '
-        + number++ 
-        + '</div><div class="line"></div></div>';
-      }
-      
-      let word_split = all_split[i].split("\n");
-      if (word_split.length === 2){ // svenska found and translation found
-        let sv_split = word_split[0].split("|").map((raw) => raw.trim());
-        let en_split = word_split[1].split("|").map((raw) => raw.trim());
-        if (sv_split.length !== en_split.length  || sv_split.length === 0) {
-          console.error("empty sv/en batch found", sv_split, en_split);
-          continue;
-        }
-        
-        filtered += '<div class="word">'
-        filtered += '<div class="container" onclick="show_hide_translation(this)">'
-        
-        for (let i = 0; i < sv_split.length; i++){
-          filtered += ''
-          + '<div class="sv">'
-          + sv_split[i]
-          + '</div><div class="en back">'
-          + en_split[i]
-          + '</div>'
-        }
-        
-        filtered += '</div>' // container
-        filtered += '<div class="extra hidden">'
-          + "sv" + '</div>' // this div should be absolute+above. Later implement voicing på svenska
-        filtered += '</div>'
-      } else {
-        console.log("parse fail. Broken word record", word_split)
-      }
-      
-    }
-  }
+  // parse
+  let all_split = content.innerHTML.trim().split("\n\n");
+  all_split = sort_words(all_split, mode);
+  console.log("all_split after sort_words", all_split, "mode after sort_words", mode);//todo remove
+  
+  let filtered = "";
+  
+  if (mode === "show all") filtered += full_data_no_sorting(all_split);
+  else filtered += only_specified_word_groups(all_split);
+  
   document.getElementById("filtered").innerHTML = filtered;
 }
 
--- sv/sort.js
+++ sv/sort.js
@@ -1,18 +1,18 @@
-const sort_modal = document.getElementById("sort-modal");
+const words_modal = document.getElementById("words-modal");
 
-function show_sort_menu(){
-  sort_modal.style.display = 'block';
+function show_words_menu(){
+  words_modal.style.display = 'block';
   document.body.classList.add('modal-open'); // Prevent scroll
 }
 
-function hide_sort_menu(){
-  sort_modal.style.display = 'none';
+function hide_words_menu(){
+  words_modal.style.display = 'none';
   document.body.classList.remove('modal-open'); //Default scrolling
 }
 
 /** all_split is string[], mode is string */
 function sort_words(all_split, mode){
-  if (mode === "original") return all_split; // no needs to change content
+  if (mode === "show all") return all_split; // no needs to change content
   // cut first x4 items from string[], since they are rules/descriptions, not a content itself
   all_split = all_split.slice(4);
   // keep only single words, so remove items started from [t]
@@ -21,11 +21,39 @@ function sort_words(all_split, mode){
   switch (mode){
     case "words abc": return sort_words_abc(all_split);
     case "words cba": return sort_words_cba(all_split);
-    case "att abc": return sort_words_abc(all_split.filter(line => line.startsWith("att ")));
-    case "att cba": return sort_words_cba(all_split.filter(line => line.startsWith("att ")));
+    case "att abc": return sort_words_abc(all_split.filter(line => line_is_att(line)));
+    case "att cba": return sort_words_cba(all_split.filter(line => line_is_att(line)));
+    case "en/ett abc": return sort_words_abc(all_split.filter(line => line_is_en_ett(line)));
+    case "en/ett cba": return sort_words_cba(all_split.filter(line => line_is_en_ett(line)));
+    case "en abc": return sort_words_abc(all_split.filter(line => line_is_en(line)));
+    case "en cba": return sort_words_cba(all_split.filter(line => line_is_en(line)));
+    case "ett abc": return sort_words_abc(all_split.filter(line => line_is_ett(line)));
+    case "ett cba": return sort_words_cba(all_split.filter(line => line_is_ett(line)));
+    case "another abc": return sort_words_abc(all_split.filter(line => line_is_another(line)));
+    case "another cba": return sort_words_cba(all_split.filter(line => line_is_another(line)));
     // todo consider en/ett filtering separetely, or when/if together en + ett then think about ignore en ett
     
   }
+}
+
+function line_is_en(line){
+  return line.startsWith("en ") && line.split("|")[0].split(" ").length === 3;
+}
+
+function line_is_ett(line){
+  return line.startsWith("ett ") && line.split("|")[0].split(" ").length === 3;
+}
+
+function line_is_en_ett(line){
+  return line_is_en(line) || line_is_ett(line);
+}
+
+function line_is_att(line){
+  return line.startsWith("att ") && line.split("|")[0].split(" ").length === 3;
+}
+
+function line_is_another(line){
+  return !line_is_att(line) && !line_is_en_ett(line);
 }
 
 /** sort all single words alphabetically, include words started from att , en , ett, another like colors and so */
